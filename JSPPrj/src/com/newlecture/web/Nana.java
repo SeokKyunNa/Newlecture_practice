0d752f8c0682aba5f2345f91e1f8e22156c2cd71
engsk <engsk1211@gmail.com>
2020-02-11 23:56:26 +0900

    Newlec servlet/JSP 15 GET requset and queryString

--- JSPPrj/src/com/newlecture/web/Nana.java
+++ JSPPrj/src/com/newlecture/web/Nana.java
@@ -22,14 +22,15 @@ public class Nana extends HttpServlet{
 	@Override
 	public void service(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 		
-		response.setCharacterEncoding("UTF-8");
-		response.setContentType("text/html; charset=UTF-8");
+		response.setCharacterEncoding("UTF-8");					// 보내는 출력 형식 지정 (인코딩 타입 지정)
+		response.setContentType("text/html; charset=UTF-8");	// 받는 콘텐츠 형식 지정
 		
 		PrintWriter out = response.getWriter();
 		
-		for(int i = 0; i < 100; i++) {
+		int cnt = Integer.parseInt(request.getParameter("cnt"));	// getParameter는 무조건 문자열로 입력되므로 상황에 맞게 캐스팅 필요
+		
+		for(int i = 0; i < cnt; i++) {
 			out.println((i + 1) + " : 안녕 Servlet!!<br />");
 		}
 	}
 }
-	
