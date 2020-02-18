00f9a067840be31afd432bda4a697795afed61b5
engsk <engsk1211@gmail.com>
2020-02-18 23:50:33 +0900

    JDBC 4 query practice

--- JDBCPrj/src/ex1/Program.java
+++ JDBCPrj/src/ex1/Program.java
@@ -1,11 +1,31 @@
 package ex1;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Program {
 	
-	public static void main(String[] args) {
-		Connection con;
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		
+		String url = "jdbc:oracle:thin:@localhost:1521/xepdb1";
+		String sql = "SELECT * FROM NOTICE";
+		
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		Connection con = DriverManager.getConnection(url, "newlec", "tjrrbs89!");
+		Statement st = con.createStatement();
+		ResultSet rs = st.executeQuery(sql);
+		
+		if(rs.next()) {		// BOF에서 시작이니까 next한 후부터 데이터를 가져옴
+			String title = rs.getString("TITLE");
+			System.out.printf("NAME : %s\n", title);
+		}
+		
+		rs.close();
+		st.close();
+		con.close();
 	}
 
 }
