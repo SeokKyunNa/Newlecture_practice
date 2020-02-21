758bcdf2c6ea846b54249ee0058dfbc58f79190c
engsk <engsk1211@gmail.com>
2020-02-21 23:59:59 +0900

    implement View for notice list commnet count

--- JSPPrj/src/com/newlecture/web/controller/NoticeListController.java
+++ JSPPrj/src/com/newlecture/web/controller/NoticeListController.java
@@ -1,7 +1,6 @@
 package com.newlecture.web.controller;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -10,7 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.newlecture.web.entity.Notice;
+import com.newlecture.web.entity.NoticeView;
 import com.newlecture.web.service.NoticeService;
 
 @WebServlet("/notice/list")
@@ -39,7 +38,7 @@ public class NoticeListController extends HttpServlet {
 		}
 		
 		NoticeService service = new NoticeService();
-		List<Notice> list = service.getNoticeList(field, query, page);
+		List<NoticeView> list = service.getNoticeList(field, query, page);
 		int count = service.getNoticeCount(field, query);
 		
 		request.setAttribute("list", list);
